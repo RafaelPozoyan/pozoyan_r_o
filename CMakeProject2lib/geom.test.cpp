a1c149636d3bfe535be13efa3b1cbab79bfe0637
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2022-12-19 15:39:49 +0300

    Update geom.test.cpp

--- CMakeProject2lib/geom.test.cpp
+++ CMakeProject2lib/geom.test.cpp
@@ -1,4 +1,4 @@
-﻿#include <geom/geom.hpp>
+#include <geom/geom.hpp>
 
 #include <iostream>
 #include <cmath>
@@ -11,8 +11,12 @@ double dot(const Rdec2D& fst, const Rdec2D& scd) {
 double norm(const Rdec2D& fst) {
 	return { sqrt(pow(fst.x, 2) + pow(fst.y, 2)) };
 }
-void polar_system(struct Rdec2D& pol) {
-	std::cout << "polar system: " << "Radious = " << sqrt(pol.x + pol.y) << ", Phi = " << tan(pol.y / pol.x) << ";" << '\n';
+void ToDec(struct Rdec2D& pol) {
+	std::cout << "Radious = " << sqrt(pol.x + pol.y) << ";" << '\n';
+	std::cout << "------------------------------------------------------------" << '\n';
+}
+void ToPol(struct Rdec2D& pol) {
+	std::cout << "Phi = " << tan(pol.y / pol.x) << ";" << '\n';
 	std::cout << "============================================================" << '\n';
 }
 //=============================================================
@@ -22,6 +26,7 @@ int main() {
 	Rdec2D z1{ 2.0, 6.0 };
 	Rdec2D z2{ 4.0, 3.0 };
 	int a = 0;
+	std::cout << "============================================================" << '\n';
 	std::cout << "enter a number:";
 	std::cin >> a;
 	std::cout << "------------------------------------------------------------" << '\n';
@@ -35,6 +40,8 @@ int main() {
 	std::cout << "------------------------------------------------------------" << '\n';
 	std::cout << "multiplication by number: z1 * a = " << z1 * a << ";" << '\n';
 	std::cout << "multiplication by number: z2 * a = " << z2 * a << ";" << '\n';
-	std::cout << "============================================================" << '\n';
-	polar_system(z1);
+	std::cout << "------------------------------------------------------------" << '\n';
+	ToDec(z1);
+	ToPol(z1);
 }
+
