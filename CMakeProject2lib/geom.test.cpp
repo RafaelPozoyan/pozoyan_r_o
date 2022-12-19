5dd5053b5f640457c1e88ab7dd0e5d0559d48fb9
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2022-12-19 14:58:08 +0300

    Add files via upload

---
+++ CMakeProject2lib/geom.test.cpp
@@ -0,0 +1,40 @@
+﻿#include <geom/geom.hpp>
+
+#include <iostream>
+#include <cmath>
+
+//-----------------------------------------
+//============================================================= 
+double skalar_product(const Rdec2D& fst, const Rdec2D& scd) {
+	return { fst.x * scd.x + fst.y * scd.y };
+}
+double vector_length(const Rdec2D& fst) {
+	return { sqrt(pow(fst.x, 2) + pow(fst.y, 2)) };
+}
+void polar_system(struct Rdec2D& pol) {
+	std::cout << "polar system: " << "Radious = " << sqrt(pol.x + pol.y) << ", Phi = " << tan(pol.y / pol.x) << ";" << '\n';
+	std::cout << "============================================================" << '\n';
+}
+//=============================================================
+// ---------------------------------------- 
+
+int main() {
+	Rdec2D z1{ 2.0, 6.0 };
+	Rdec2D z2{ 4.0, 3.0 };
+	int a = 0;
+	std::cout << "enter a number:";
+	std::cin >> a;
+	std::cout << "------------------------------------------------------------" << '\n';
+	std::cout << "vector z1:" << z1 << '\n';
+	std::cout << "vector z2:" << z2 << '\n';
+	std::cout << "------------------------------------------------------------" << '\n';
+	std::cout << "scalar product = " << skalar_product(z1, z2) << ";" << '\n';
+	std::cout << "------------------------------------------------------------" << '\n';
+	std::cout << "vector z1 length = " << vector_length(z1) << ";" << '\n';
+	std::cout << "vector z2 length = " << vector_length(z2) << ";" << '\n';
+	std::cout << "------------------------------------------------------------" << '\n';
+	std::cout << "multiplication by number: z1 * a = " << z1 * a << ";" << '\n';
+	std::cout << "multiplication by number: z2 * a = " << z2 * a << ";" << '\n';
+	std::cout << "============================================================" << '\n';
+	polar_system(z1);
+}
---
+++ CMakeProject2lib/geom/geom.cpp
@@ -0,0 +1,28 @@
+﻿#include "geom.hpp"
+
+#include <iostream>
+#include <cmath>
+
+//------------------------------------------
+//============================================================= ОПРЕДЕЛЕНИЕ ФУНКЦИИ
+std::ostream& operator  <<(std::ostream& ostrm, const Rdec2D& z) { /*оператор вывода Декартовой системы*/
+	return ostrm << "(" << z.x << "," << z.y << ")";
+}
+
+std::ostream& operator  <<(std::ostream& ostrm, const Rpol2D& z) { /*оператор вывода полярнной системы*/
+	return ostrm << z.r << "," << z.phi;
+}
+
+Rdec2D operator*(const Rdec2D& fst, const double& a) {
+	return { fst.x * a, fst.y * a };
+}
+
+Rdec2D operator-(const Rdec2D& fst, const Rdec2D& scd) {
+	return { fst.x - scd.x, fst.y - scd.y };
+}
+
+Rdec2D operator+(const Rdec2D& fst, const Rdec2D& scd) {
+	return { fst.x + scd.x, fst.y + scd.y };
+}
+//============================================================= ОПРЕДЕЛЕНИЕ ФУНКЦИИ
+//-----------------------------------------
