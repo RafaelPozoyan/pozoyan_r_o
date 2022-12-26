961257886c5c5bfdd1a05a6923e17aa9d1c527f9
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2022-12-26 22:45:46 +0300

    Update bpm_22_4_3.cpp

--- bpm_22_4_3.cpp
+++ bpm_22_4_3.cpp
@@ -1,48 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main() {
-	int cnt = 0;
-	std::string w1 = "westy";
-	std::string w2 = "test";
-	if (w1.length() == w2.length()) {
-		int flag = 1;
-        while (flag == 1) {
-		for (int i = 0; i < w1.length(); i += 1) {
-				if (w1[i] == w2[i]) {
-					flag = 1;
-				}
-				else {
-					cnt += 2;
-					flag = 0;
-				}
-                flag = 0;
-			}
-        std::cout << cnt;
-		}
-	}
-    //слова разной длины не работают
-    else {
-        int flag = 1;
-        while (flag == 1) {
-            for (int i = 0; i < std::max(w1.length(), w2.length()); i += 1) {
-                for (int j = 0; j < std::min(w2.length(), w2.length()); j += 1) {
-                    if (w1[j] == w2[j]) {
-                        flag = 1;
-                        break;
-                    } else {
-                        flag = 0;
-                        cnt += 1;
-                    }
-                }
-                if(flag == 0){
-                    cnt += 1;
-                }
-                flag = 0;
+int main(){
+    std::string w1, w2;
+    std::cin >> w1 >> w2;
+    int flag = 1;
+    int cnt = 0;
+    for(int i=std::max(w1.length(), w2.length()); i>=0; i--){
+        if (w1[i] == w2[i]){
+            flag = 1;
+        }
+        else{
+            if (w1.length() != w2.length()) {
+                cnt += 1;
             }
-            std::cout << cnt << '\n';
+            else{
+                cnt += 2;
+            }
         }
     }
-
-    //слова разной длины не работают
+    std::cout << cnt;
 }
